d80c849850edcf376a8a5ed6e7774d99737fe6ca
yakim shu <yakim-shu@users.noreply.github.com>
2020-04-05 22:54:42 +0800

    1. Add header 2. Add font awesome

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,23 @@ const sass = require('gulp-sass');
 const minifyCSS = require('gulp-csso');
 const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
+const livereload = require('gulp-livereload');
 
 function html() {
-  return src('src/html/**/*.pug')
+  return src('src/html/*.pug')
     .pipe(pug())
-    .pipe(dest('public/html'))
+    .pipe(dest('public'))
+    .pipe(livereload())
 }
 
 function css() {
-  return src('src/scss/**/*.scss')
+  return src('src/scss/*.scss')
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(minifyCSS())
     .pipe(sourcemaps.write())
     .pipe(dest('public/css'))
+    .pipe(livereload())
 }
 
 function js() {
@@ -28,12 +31,13 @@ function js() {
 }
 
 function watchFiles() {
+  livereload.listen(5500);
   gulp.watch("./src/scss/**/*", css);
   gulp.watch("./src/html/**/*", html);
 }
 
 const watch = gulp.parallel(watchFiles);
-const build = gulp.series(html, css, js, watch);
+const build = gulp.series(watch, html, css, js);
 
 exports.js = js;
 exports.css = css;
