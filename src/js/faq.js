79dff2d205932d9abd03215296d6294b1720c7b5
yakim shu <yakim-shu@users.noreply.github.com>
2020-04-12 01:52:14 +0800

    Add course-finfo page

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   $(".faq__list__item").click(function (e) {
     e.preventDefault()
-    $(this).siblings().removeClass("active")
-    $(this).toggleClass("active")
+    $(this).toggleClass("active").siblings().removeClass("active")
   })
 });
