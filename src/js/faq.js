dc2b676cac85de194712f8af9c5d168d08c08a97
yakim shu <yakim-shu@users.noreply.github.com>
2020-04-11 17:20:38 +0800

    Adjust faq animation

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,9 +1,7 @@
-$(document).ready(function(){
-  $(".faq__list > .faq__list__item.active").children(".faq__list__item-content").slideDown();
-
-  $(".faq__list > .faq__list__item").click(function(e) {
+$(document).ready(function () {
+  $(".faq__list__item").click(function (e) {
     e.preventDefault()
-    $(".faq__list__item").removeClass("active").children(".faq__list__item-content").slideUp("slow");
-    $(this).toggleClass("active").children(".faq__list__item-content").slideToggle("slow")
+    $(this).siblings().removeClass("active")
+    $(this).toggleClass("active")
   })
 });
