a3f072b08fdccf0b146bced4cc49d4939d13e695
ChihYang41 <Jigsaw0401@gmail.com>
2020-04-12 22:40:38 +0800

    Simplified header.js code structure &  Adjust header css

--- src/js/header.js
+++ src/js/header.js
@@ -1,28 +1,23 @@
 $(document).ready(function() {
   $(document).scroll(function() {
     var scrollHeight = $(document).scrollTop();
-    if(scrollHeight > 20) {
+    if(scrollHeight > 120) {
       $(".header").addClass("is-shrunk");
-      $(".header__cta-btn").addClass("off")
-      $("nav").addClass("off")
+      $(".header__cta-btn, nav").addClass("off")
     } else {
-      
       $(".header").removeClass("is-shrunk");
      
       setTimeout(function() {
-        $(".header__cta-btn").removeClass("off")
-        $("nav").removeClass("off")
+        $(".header__cta-btn, nav").removeClass("off")
       }, 350);
     }
   })
 
   $(".header__responsive__icon").click(function() {
-    $(".header__menu").addClass("is-opened")
-    $("body").addClass("overflow-hidden")
+    $("body").addClass("is-menu-opened")
   })
 
   $(".header__menu__close-btn").click(function() {
-    $(".header__menu").removeClass("is-opened")
-    $("body").removeClass("overflow-hidden")
+    $("body").removeClass("is-menu-opened")
   })
 })
